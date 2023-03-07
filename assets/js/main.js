9eb17c28c50e5206feed8788d4f71dbb09cf4da8
Ismael Cézar <ismaelcz4r@gmail.com>
2023-03-07 00:20:02 -0300

    Meu primeiro portifolio em HTML, CSS e JS

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+
+/*===== BARRA DE NAVEGACAO ATIVA  =====*/
+const navAtiva = document.querySelectorAll('.nav_descricao--link');
+
+function ativa () {
+  navAtiva.forEach(link => link.classList.remove('active'))
+  this.classList.add('active')
+};
+
+
+navAtiva.forEach(n => n.addEventListener("click",ativa))
+
+/*=====  ANIMAÇÃO SCROLL =====*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+/*===== SCROLL HOME =====*/
+sr.reveal('.home_titulo',{})
+sr.reveal('.home_botao',{})
+sr.reveal('home_marcas',{})
+sr.reveal('.nav_marcas--items',{})
+sr.reveal('.home_foto',{})
+
+/*===== SCROLL ABOUR =====*/
+sr.reveal('.about_titulo',{})
+sr.reveal('.about_subtitulo',{})
+sr.reveal('.about_texto',{})
+sr.reveal('.about_botao',{})
+sr.reveal('.about_foto',{})
+sr.reveal('.ab',{})
+
+/*===== SCROLL SKILLS =====*/
+sr.reveal('.skills_titulo',{})
+sr.reveal('.skills_subtitulo',{})
+sr.reveal('.skills_stack',{})
+sr.reveal('.skills_texto',{})
+sr.reveal('.skill_image',{})
+
+/*===== SCROLL PROJECTOS =====*/
+sr.reveal('.pojetos_titulo',{})
+sr.reveal('.projetos_imagens',{})
+
+/*===== SCROLL CONTACT =====*/
+sr.reveal('.contato_titulo',{})
+sr.reveal('.contato_texto',{})
+sr.reveal('.contato_container',{})
+sr.reveal('.contato_botao-link',{})
+sr.reveal('.skill_image',{})
